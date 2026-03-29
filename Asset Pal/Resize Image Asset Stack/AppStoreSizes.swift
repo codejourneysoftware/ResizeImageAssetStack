c48b139c3b6b470c60af8d502e4a627c440ba76d
Isaac Ortiz <isaacortiz@Isaacs-Mac-mini.local>
2026-03-29 05:12:00 -0700

    Feat: Add macOS screenshot target support with four required dimensions

--- Asset Pal/Resize Image Asset Stack/AppStoreSizes.swift
+++ Asset Pal/Resize Image Asset Stack/AppStoreSizes.swift
@@ -4,6 +4,7 @@ import CoreGraphics
 enum AppStoreDevice: String, CaseIterable, Identifiable {
     case iphone = "IPHONE"
     case ipad = "IPAD"
+    case mac = "MAC"
     var id: String { rawValue }
 }
 
@@ -24,6 +25,11 @@ struct AppStoreTargetSizes {
         AppStoreSize(name: "iPad 13in (2064x2752)", device: .ipad, size: CGSize(width: 2064, height: 2752)),
         AppStoreSize(name: "iPad 13in (2752x2064)", device: .ipad, size: CGSize(width: 2752, height: 2064)),
         AppStoreSize(name: "iPad 12.9in (2048x2732)", device: .ipad, size: CGSize(width: 2048, height: 2732)),
-        AppStoreSize(name: "iPad 12.9in (2732x2048)", device: .ipad, size: CGSize(width: 2732, height: 2048))
+        AppStoreSize(name: "iPad 12.9in (2732x2048)", device: .ipad, size: CGSize(width: 2732, height: 2048)),
+        
+        AppStoreSize(name: "Mac 1280x800", device: .mac, size: CGSize(width: 1280, height: 800)),
+        AppStoreSize(name: "Mac 1440x900", device: .mac, size: CGSize(width: 1440, height: 900)),
+        AppStoreSize(name: "Mac 2560x1600", device: .mac, size: CGSize(width: 2560, height: 1600)),
+        AppStoreSize(name: "Mac 2880x1800", device: .mac, size: CGSize(width: 2880, height: 1800))
     ]
 }
